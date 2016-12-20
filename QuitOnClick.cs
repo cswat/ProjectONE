ab0d7f13adf56e04ffc7ef2a485d1860cd2519f2
cswat <connor.swatling+gh@gmail.com>
2016-12-19 20:50:07 -0700

    12.19.16 Update
    Main menu functionality and imported assets

---
+++ QuitOnClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitOnClick : MonoBehaviour {
+
+	public void Quit() //needs to be public for OnClick event
+	{
+#if UNITY_EDITOR //platform specific - if run in unity...
+		UnityEditor.EditorApplication.isPlaying = false; //...close play mode...
+#else //..otherwise, quit application.
+		Application.Quit();
+#endif
+	}//Quit
+
+}
